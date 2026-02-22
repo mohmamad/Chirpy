a12c32e194dc313f9f3c05d0287c819ed2a58573
mohmamad <mohamad.isa.moghrabi@gmail.com>
2026-02-22 19:54:04 +0200

    add readiness endpoint

---
+++ src/api/readiness.ts
@@ -0,0 +1,5 @@
+import express from "express";
+import { Request, Response } from "express";
+export function handlerReadiness(req: Request, res: Response) {
+  return res.status(200).type("text/plain; charset=utf-8").send("OK");
+}
