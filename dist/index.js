f217df9e75974c46eb4136304e6e45205a170fbf
mohmamad <mohamad.isa.moghrabi@gmail.com>
2026-02-22 23:23:17 +0200

    add loggin midlleware

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { handlerReadiness } from "./api/readiness.js";
+import { middlewareLogResponses } from "./logMiddleware.js";
 const app = express();
 const PORT = 8080;
+app.use(middlewareLogResponses);
 app.use("/app", express.static("./src/app"));
-app.get("/healthz", handlerReadiness);
+app.get("/healthz", middlewareLogResponses, handlerReadiness);
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
